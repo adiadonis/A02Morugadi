398f207d1b9789ca7a76ac9b0963069c3ec21ea5
adiadonis <hellraiseradonis@gmail.com>
2016-09-23 23:31:45 -0500

    Add files via upload

---
+++ meter.js
@@ -0,0 +1,10 @@
+
+function calculate() {
+var a = document.getElementById("value1").value;
+var b =  document.getElementById("value2").value;
+
+var x=a*b;
+  document.getElementById("result").innerHTML=x;
+}
+
+
